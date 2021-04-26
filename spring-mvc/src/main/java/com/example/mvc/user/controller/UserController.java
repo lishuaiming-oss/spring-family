801c38cd46bd77b0a55d1375bc03f86727e3891f
javakc_global <javakc_global@javakc.com>
2021-04-26 17:23:33 +0800

    第二次提交 进度 springmvc

--- spring-mvc/src/main/java/com/example/mvc/user/controller/UserController.java
+++ spring-mvc/src/main/java/com/example/mvc/user/controller/UserController.java
@@ -1,8 +1,11 @@
 package com.example.mvc.user.controller;
 
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
-import org.springframework.web.servlet.mvc.Controller;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -13,9 +16,30 @@ import javax.servlet.http.HttpServletResponse;
  * @Author: li shuai ming
  * @Create: 2021/4/23-16:32
  */
-public class UserController implements Controller {
-    @Override
-    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
-        return null;
+@Controller
+@RequestMapping("user")
+public class UserController {
+  //基本配置bean注入
+//  构造器注入
+    /*public UserController() {
+        System.out.println("controller init");
     }
+*/
+ /*   @Override
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+        System.out.println("userController handleRequest !");
+        return new ModelAndView("sucess");
+    }
+}
+*/
+ //注解实现类路径关联
+    @RequestMapping(value = "create.do")
+    public ModelAndView create(@RequestParam String name){
+        //System.out.println("userController login method!");
+        System.out.println("String name:"+name);
+       // System.out.println("int age:"+age);
+        //System.out.println("int age:");
+        return new ModelAndView("sucess");
+    }
+
 }
